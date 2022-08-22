3e412b68c67568f931600a6ca57374888edddd41
moromin <moromin933@gmai.com>
2022-08-22 22:25:10 +0900

    feat: managing data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,21 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'cart', component: CartComponent},
+      { path: 'shipping', component: ShippingComponent},
     ]),
   ],
   declarations: [
@@ -24,6 +31,8 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
+    CartComponent,
+    ShippingComponent,
   ],
   bootstrap: [AppComponent],
 })
